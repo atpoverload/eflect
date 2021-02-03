ba4a6f1affde75e0ba9ed45872c9a477ee09742f
timurbey <atpoverload@gmail.com>
2021-02-02 21:28:17 -0700

    fixed hang issue when reading /proc/pid/stat

--- java/eflect/util/ProcUtil.java
+++ java/eflect/util/ProcUtil.java
@@ -2,30 +2,23 @@ package eflect.util;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
 /** Utility to access data from /proc/ */
-// TODO(timur): this should be moved to java/eflect/data/jiffies
+// TODO(timur): should this be moved to java/eflect/data/jiffies?
 public class ProcUtil {
   // /proc/pid/task
   private static final long PID = ProcessHandle.current().pid();
 
-  public static String[] getTasks() {
-    return new File(String.join(File.separator, "/proc", Long.toString(PID), "task")).list();
-  }
-
+  /** Reads this application's thread's jiffies stat files. */
   public static ArrayList<String> readTaskStats() {
     ArrayList<String> stats = new ArrayList<String>();
     File tasks = new File(String.join(File.separator, "/proc", Long.toString(PID), "task"));
     for (File task : tasks.listFiles()) {
-      File stat = new File(task, "stat");
-      if (!stat.canRead()) {
-        continue;
-      }
-      try (BufferedReader reader = new BufferedReader(new FileReader(stat))) {
-        // TODO(timur): fix the hang that occurs here
-        stats.add(reader.readLine());
+      try {
+        stats.add(Files.readString(Path.of(task.getPath(), "stat")));
       } catch (Exception e) {
         e.printStackTrace();
       }
@@ -39,7 +32,7 @@ public class ProcUtil {
 
   public static String[] readProcStat() {
     String[] stats = new String[CPU_COUNT];
-    try (BufferedReader reader = new BufferedReader(new FileReader(new File(SYSTEM_STAT_FILE)))) {
+    try (BufferedReader reader = Files.newBufferedReader(Path.of(SYSTEM_STAT_FILE))) {
       reader.readLine(); // first line is total summary; we need by cpu
       for (int i = 0; i < CPU_COUNT; i++) {
         stats[i] = reader.readLine();
