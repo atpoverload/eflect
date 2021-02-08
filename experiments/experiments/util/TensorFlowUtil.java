045539fdb269f0cbeb90f5edade0fe3db53caab4
timurbey <atpoverload@gmail.com>
2021-02-08 13:32:43 -0700

    checkpointing tf driver

---
+++ experiments/experiments/util/TensorFlowUtil.java
@@ -0,0 +1,60 @@
+package eflect.experiments.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.tensorflow.Graph;
+import org.tensorflow.Output;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+
+public class TensorFlowUtil {
+  public static byte[] readBytes(Path path) {
+    try {
+      return Files.readAllBytes(path);
+    } catch (IOException e) {
+      System.err.println("Failed to read [" + path + "]: " + e.getMessage());
+      System.exit(1);
+    }
+    return null;
+  }
+
+  public static Tensor<Float> normalizeImage(byte[] imageBytes) {
+    try (Graph g = new Graph()) {
+      GraphBuilder b = new GraphBuilder(g);
+      // Some constants specific to the pre-trained model at:
+      // https://storage.googleapis.com/download.tensorflow.org/models/inception5h.zip
+      //
+      // - The model was trained with images scaled to 224x224 pixels.
+      // - The colors, represented as R, G, B in 1-byte each were converted to
+      //   float using (value - Mean)/Scale.
+
+      // replace with parameters?
+      final int H = 224;
+      final int W = 224;
+      final float mean = 117f;
+      final float scale = 1f;
+
+      // Since the graph is being constructed once per execution here, we can use a constant for the
+      // input image. If the graph were to be re-used for multiple input images, a placeholder would
+      // have been more appropriate.
+      final Output<String> input = b.constant("input", imageBytes);
+      final Output<Float> output =
+          b.div(
+              b.sub(
+                  b.resizeBilinear(
+                      b.expandDims(
+                          b.cast(b.decodeJpeg(input, 3), Float.class), b.constant("make_batch", 0)),
+                      b.constant("size", new int[] {H, W})),
+                  b.constant("mean", mean)),
+              b.constant("scale", scale));
+      try (Session s = new Session(g)) {
+        // Generally, there may be multiple output tensors, all of them must be closed to prevent
+        // resource leaks.
+        return s.runner().fetch(output.op().name()).run().get(0).expect(Float.class);
+      }
+    }
+  }
+
+  private TensorFlowUtil() {}
+}
