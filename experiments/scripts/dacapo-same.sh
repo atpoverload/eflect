e41656cef44deab3dea6bc5393c08b1dfc8e45f2
timurbey <atpoverload@gmail.com>
2021-07-05 08:56:30 -0600

    fixed dacapo experiment data pathing

--- experiments/scripts/dacapo-same.sh
+++ experiments/scripts/dacapo-same.sh
@@ -5,9 +5,12 @@ SIZE=$2
 RUNS=$3
 SCRATCH_ROOT=scratch
 
+# i did this stupidly
+OUTPUT_DIR=$DATA_DIR
 pids=""
 for i in `seq 1 1 $RUNS`; do
-  scripts/dacapo.sh $BENCHMARK -s $SIZE -n 20 --scratch-directory $SCRATCH_ROOT/$i &
+  export DATA_DIR=$OUTPUT_DIR/$i
+  scripts/dacapo.sh $BENCHMARK -s $SIZE -n 1 --scratch-directory $SCRATCH_ROOT/$i &
   pids+=$!" "
 done
 
